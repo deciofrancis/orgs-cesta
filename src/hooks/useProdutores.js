ba1273cf0d92a370ecc7e8eca3340a1e6d546943
Décio Francis <francisdasilva@gmail.com>
2023-02-14 21:32:21 -0300

    implementando melhores produtores

--- src/hooks/useProdutores.js
+++ src/hooks/useProdutores.js
@@ -1,20 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-import { carregaProdutores } from "../servicos/carregaDados";
+import { carregaProdutores } from '../servicos/carregaDados';
 
-export default function useProdutores() {
-
-    const [titulo, setTitulo] = useState('');
-    const [lista, setLista] = useState('');
+export default function useProdutores(melhoresProdutores) {
+    const [lista, setLista] = useState([]);
 
     useEffect(() => {
         const retorno = carregaProdutores();
         retorno.lista.sort(
             (produtor1, produtor2) => produtor1.distancia - produtor2.distancia,
         );
-        setTitulo(retorno.titulo);
-        setLista(retorno.lista);
+        let novaLista = retorno.lista;
+        
+        if (melhoresProdutores) {
+            novaLista = novaLista.filter(
+                (produtor) => produtor.estrelas > 3
+            );
+        }
+        setLista(novaLista);
     }, []);
 
-    return [titulo, lista];
+    return lista;
 }
---
+++ src/telas/Produtores/componentes/Produtores.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { FlatList, Text, StyleSheet } from 'react-native';
+
+import Produtor from './Produtor';
+import useProdutores from "../../../hooks/useProdutores";
+
+
+export default function Produtores( {topo: Topo} ) {
+
+    const [titulo, lista] = useProdutores(); 
+
+    const TopoLista = () => {
+        return <>
+            <Topo />
+            <Text style={estilos.titulo}>{ titulo }</Text>
+        </>
+        
+    }
+
+    return <FlatList
+            data={lista}
+            renderItem={({item}) => <Produtor {...item} />}
+            keyExtractor={({ nome }) => nome}
+            ListHeaderComponent={TopoLista}
+            />
+}
+
+const estilos = StyleSheet.create({
+    titulo: {
+        fontSize: 20,
+        lineHeight: 32,
+        marginHoizontal: 16,
+        marginTop: 16,
+        fontHeight: 'bold',
+        color: '#464646',
+    }
+});
