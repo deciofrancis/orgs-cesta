e92f78b759048fb5f0b482ee052a394e45e8aa23
Décio Francis <francisdasilva@gmail.com>
2023-02-26 12:16:29 -0300

    corrigindo rota cesta e melhorando detalhes

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,56 +1,71 @@
-import React from "react";
-import { StyleSheet, View, Image } from "react-native";
+import React from 'react';
+import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
 
+import useTextos from '../../../hooks/useTextos';
 import Texto from '../../../componentes/Texto';
-import Botao from '../../../componentes/BotaoGreen';
 
-export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
-    return <>
-        <Texto style={estilos.nome}>{ nome }</Texto>
-        <View style={estilos.fazenda}>
-            <Image source={logoFazenda} style={estilos.imagemFazenda} />
-            <Texto style={estilos.nomeFazenda}>{ nomeFazenda }</Texto>
-        </View>
-        <Texto style={estilos.descricao}>{ descricao }</Texto>
-        <Texto style={estilos.preco}>{ preco }</Texto>
+export default function Detalhes({ nome, produtor, descricao, preco }) {
+  const { botaoComprar } = useTextos();
 
-        <Botao texto={botao} style={estilos.botao} onPress={() => {}} />
-    </>
+  return <>
+    <View style={estilos.fazenda}>
+      <Image source={produtor.imagem} style={estilos.imagemFazenda} />
+      <Texto style={estilos.nomeFazenda}>{ produtor.nome }</Texto>
+    </View>
+    <Texto style={estilos.descricao}>{ descricao }</Texto>
+    <Texto style={estilos.preco}>{ preco }</Texto>
+
+    <TouchableOpacity 
+      style={estilos.botao} 
+      onPress={() => {}}>
+      <Texto style={estilos.textoBotao}>{ botaoComprar }</Texto>
+    </TouchableOpacity>
+  </>
 }
 
 const estilos = StyleSheet.create({
-    nome: {
-        color: "#464646",
-        fontSize: 26,
-        lineHeight: 42,
-        fontWeight: "bold",
-    },
-    fazenda:{
-        flexDirection: "row",
-        lineHeight: 26,
-    },
-    imagemFazenda: {
-        width: 32,
-        height: 32,
-    },
-    nomeFazenda: {
-        fontSize: 16,
-        lineHeight: 26,
-        marginLeft: 12,
-    },
-    descricao: {
-        color: "#A3A3A3",
-        fontSize: 16,
-        lineHeight: 26,
-    },
-    preco: {
-        color: "#2A9F85",
-        fontWeight: "bold",
-        fontSize: 26,
-        lineHeight: 42,
-        marginTop: 8,
-    },
-    botao: {
-        marginTop: 16,
-    }
+  nome: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: 'bold',
+  },
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+  imagemFazenda: {
+    width: 32,
+    height: 32,
+  },
+  nomeFazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+  },
+  descricao: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+  preco: {
+    color: "#2A9F85",
+    fontWeight: "bold",
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8,
+  },
+  botao: {
+    marginTop: 16,
+    backgroundColor: "#2A9F85",
+    paddingVertical: 16,
+    borderRadius: 6,
+  },
+  textoBotao: {
+    textAlign: "center",
+    color: "#ffffff",
+    fontSize: 16,
+    lineHeight: 26,
+    fontWeight: "bold",
+  },
 })
